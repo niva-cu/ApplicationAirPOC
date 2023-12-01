b52bdacd2c0a83c2ae2d22c91500ca5b6d6f2883
Niva Das <Niva.Das@neudesic.com>
2023-12-01 17:17:53 +0530

    MyActivities UI updated

--- ApplicationAirPOC/app/src/main/java/com/nivs/applicationairpoc/MyActivityScreen.kt
+++ ApplicationAirPOC/app/src/main/java/com/nivs/applicationairpoc/MyActivityScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,8 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
@@ -29,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,11 +59,33 @@ fun MyActivityPreview() {
         .background(color = AppBackgroundLightGray)
         .fillMaxWidth()) {
 
-        Image(painter = painterResource(id = R.drawable.and1),
+        /*Image(painter = painterResource(id = R.drawable.and1),
             contentDescription = "",
             modifier = Modifier
                 .fillMaxHeight(.2f)
-                .align(Alignment.CenterHorizontally))
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop)*/
+
+        Card(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.2f)
+                .padding(20.dp, 16.dp,20.dp,16.dp),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.images),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f),
+                contentScale = ContentScale.Crop
+            )
+        }
+
 
         TabBarLayout()
     }
